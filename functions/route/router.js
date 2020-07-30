267530fbafadf25b4b41814bb5d0d03d5480f68b
winnerkkh88 <winnerkkh88@gmail.com>
2020-07-29 23:09:47 -0400

    Implementation of routing to slidesData

--- functions/route/router.js
+++ functions/route/router.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const route = express.Router();
 
-//geting my experiences as an object
+//getting my experiences as an object
 const experiences = require("../controller/experiences");
+
 //do not need a function call sign such as ()
 route.route("/experiences").get(experiences.getExperiences);
 
 //Send mail to gmail for the feedback
 const sendFeedbackEmail = require("../controller/sendFeedbackEmail");
 route.route("/sendFeedbackEmail").post(sendFeedbackEmail.feedbackHandle);
+
+//getting slideshow data
+const slidesData = require("../controller/slidesData");
+route.route("/slidesData").get(slidesData.getSlidesData);
 
 module.exports = route;
