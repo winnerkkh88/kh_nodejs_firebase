11dfbfbcb023bc922c0b58eed7f59c2053da1f6a
winnerkkh88 <winnerkkh88@gmail.com>
2020-07-29 23:10:01 -0400

    Implementation of slidesData

---
+++ functions/controller/slidesData.js
@@ -0,0 +1,80 @@
+exports.getSlidesData = (req, res) => {
+  const getSlidesData = {
+    overallPages: {
+      pageTitle: "Overall Portfolio Pages",
+      webpageUiLinks: [
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fhome%2Fhome.PNG?alt=media&token=1b55e824-c2d6-4a9d-8f8e-2ca47419185a",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme.PNG?alt=media&token=daf52fba-b97e-480a-bc90-b3bd20ea824e",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme2.PNG?alt=media&token=359ed2e6-90ef-4d85-b6bb-94152779f5bc",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme3.PNG?alt=media&token=8f48adb0-3d7a-4538-ab01-a6cf098df17d",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fcontactme%2Fcontactme.PNG?alt=media&token=5e89d8d3-6d55-409d-b900-014f847c0b3b",
+      ],
+      webLegendContents: [
+        "Home",
+        "About Me1",
+        "About Me2",
+        "About Me3",
+        "Contact Me",
+      ],
+      mobileUiLinks: [
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fhome%2Fhome-mobile.PNG?alt=media&token=bba19498-f9ee-4fc1-bfe2-829e28bf1dc9",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fhome%2Fhome-mobile2.PNG?alt=media&token=ba72ca60-dc83-4b10-859a-d53e8d2219a3",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fhome%2Fhome-mobile3.PNG?alt=media&token=6a9d2068-b3f9-402a-b2b4-b7924905fb43",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme-mobile.PNG?alt=media&token=ad515c15-6565-4b23-97d6-e90492a67902",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme-mobile2.PNG?alt=media&token=b86e577a-1502-4f5d-b82d-27ded4b68823",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme-mobile3.PNG?alt=media&token=c35d782c-0aef-4349-be51-0b0a00c9ebf8",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme-mobile4.PNG?alt=media&token=fd9d0715-0c65-4da1-a070-0c8ce98fb849",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Faboutme%2Faboutme-mobile5.PNG?alt=media&token=8f5c2804-a181-4c73-9b9b-47726b3b80bd",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fcontactme%2Fcontactme-mobile.PNG?alt=media&token=f4094fca-75c4-4baf-ac13-719aa3162ef4",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fcontactme%2Fcontactme-mobile2.PNG?alt=media&token=113e3c97-9e3e-43f3-8750-650a6df72439",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Fcontactme%2Fcontactme-mobile3.PNG?alt=media&token=9936588a-91ca-422a-9807-83fba31e34f0",
+      ],
+
+      mobileLegendContents: [
+        "Home - Mobile1",
+        "Home - Mobile2",
+        "Home - Mobile3",
+        "About Me - Mobile1",
+        "About Me - Mobile2",
+        "About Me - Mobile3",
+        "About Me - Mobile4",
+        "About Me - Mobile5",
+        "Contact Me - Mobile1",
+        "Contact Me - Mobile2",
+        "Contact Me - Mobile3",
+      ],
+    },
+
+    movieList: {
+      pageTitle: "TOP 20 Movies Download",
+      webpageUiLinks: [
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist.PNG?alt=media&token=11d0c3b6-79f6-4fae-965e-a50c89fa69b7",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist2.PNG?alt=media&token=476ee9aa-d698-40ff-843c-c99f18288729",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist3.PNG?alt=media&token=453f0f62-53e9-4a20-a74b-dde52100c713",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist4.PNG?alt=media&token=505bc678-a2fe-49a5-843e-ab7ea8c157fb",
+      ],
+      webLegendContents: [
+        "Movie List1",
+        "Movie List2",
+        "Movie List3",
+        "Movie List4",
+      ],
+      mobileUiLinks: [
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist-mobile.PNG?alt=media&token=be5c019a-45b5-4927-b536-068b10ab26c8",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist-mobile2.PNG?alt=media&token=60e3a7a1-94a1-4b94-85e6-2dc99f462c0c",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist-mobile3.PNG?alt=media&token=0f28778e-2b0d-4fc9-8a95-81ce2bacb496",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist-mobile4.PNG?alt=media&token=90f16e9c-dcdb-44a9-aca3-60126cd1b8fa",
+        "https://firebasestorage.googleapis.com/v0/b/node-kh.appspot.com/o/images%2FUI-images%2Ftop20movielist%2Ftop20movielist-mobile5.PNG?alt=media&token=39284579-d3b3-4419-b931-032f9b1251af",
+      ],
+
+      mobileLegendContents: [
+        "Movie List - Mobile1",
+        "Movie List - Mobile2",
+        "Movie List - Mobile3",
+        "Movie List - Mobile4",
+        "Movie List - Mobile5",
+      ],
+    },
+  };
+  res.send(getSlidesData);
+};
