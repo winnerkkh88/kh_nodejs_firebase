7a6dd2d4c905edb57db6dc32005bfbf5f74a82e8
winnerkkh88 <winnerkkh88@gmail.com>
2020-08-14 13:03:43 -0400

    Added routing for membership

--- functions/route/router.js
+++ functions/route/router.js
@@ -2,9 +2,8 @@ const express = require("express");
 const route = express.Router();
 
 //getting my experiences as an object
-const experiences = require("../controller/experiences");
-
 //do not need a function call sign such as ()
+const experiences = require("../controller/experiences");
 route.route("/experiences").get(experiences.getExperiences);
 
 //Send mail to gmail for the feedback
@@ -14,5 +13,9 @@ route.route("/sendFeedbackEmail").post(sendFeedbackEmail.feedbackHandle);
 //getting slideshow data
 const slidesData = require("../controller/slidesData");
 route.route("/slidesData").get(slidesData.getSlidesData);
+
+//getting membership data from Realtime Database of Firstbase
+const membership = require("../controller/db/membership");
+route.route("/membership").get(membership.membership);
 
 module.exports = route;
