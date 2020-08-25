d3e6b742795d54c108a96de261ad0963df8ebef4
winnerkkh88 <winnerkkh88@gmail.com>
2020-08-25 12:25:00 -0400

    Cors for request method added

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,8 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "*");
+  //res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   next();
 });
 
